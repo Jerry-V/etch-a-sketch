846d37b08887b6b8f177addc02f18944e5cb159d
Yolo <yolo@fakeemail.com>
2022-07-08 12:52:17 -0600

    Scale the grid appropriately given input

--- javascript.js
+++ javascript.js
@@ -1,27 +1,39 @@
 
+// needs to be linked to button
+let width = 2;
+let nCells = width * width;
+
 // reference site for creating a grid layout:
 // https://www.w3schools.com/css/css_grid.asp
+// grid layout cheatsheets
+// https://grid.malven.co/
+// https://chriskonings.com/side/grid-cheat-sheet/
+// grid tutorials
+// https://cssgridgarden.com/
 const body = document.body;
 let container = document.querySelector('#container');
-container.style = 'width: 500px; height: 500px;';
-//container.style = 'display: inline-grid; gap: 50px 50px;'
+container.style = 'width: 500px; height: 500px; display: grid; grid-template-columns: repeat('+width+', 1fr);';
 container.style.backgroundColor = 'white';
 container.style.border = 'solid black 5px';
+
 
 let cell = document.createElement('div');
 cell.className='cell-item';
 cell.textContent='123';
-cell.style = 'width: 100px; height: 100px; display: flex; justify-content: center; align-items: center;';
+cell.style = 'display: flex; justify-content: center; align-items: center;';
 cell.style.backgroundColor='lightgrey';
 cell.style.border = 'solid 5px grey';
 cell.style.borderRadius = '5px';
 
-let nCells = 20;
+
 
 // without cloneNode set to true to add multiple elements
 // https://javascript.tutorialink.com/how-to-appendchildelement-many-times-the-same-element/
-container.appendChild(cell.cloneNode(true));
-container.appendChild(cell.cloneNode(true));
+for (i=0; i<nCells; i++){
+    // number each cell
+    cell.textContent=''+(i+1)+''
+    container.appendChild(cell.cloneNode(true));
+}
 
 // add eventListener to multiple items
 // https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
