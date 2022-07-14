867d83eee4981422a54524ae05b43a06f8d37b83
Yolo <yolo@fakeemail.com>
2022-07-14 15:44:56 -0600

    Finish button functionality

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-
-// needs to be linked to button
-let width = 2;
-let nCells = width * width;
-
 // reference site for creating a grid layout:
 // https://www.w3schools.com/css/css_grid.asp
 // grid layout cheatsheets
@@ -10,35 +5,34 @@ let nCells = width * width;
 // https://chriskonings.com/side/grid-cheat-sheet/
 // grid tutorials
 // https://cssgridgarden.com/
-const body = document.body;
+
 let container = document.querySelector('#container');
-container.style = 'width: 500px; height: 500px; display: grid; grid-template-columns: repeat('+width+', 1fr);';
-container.style.backgroundColor = 'white';
-container.style.border = 'solid black 5px';
-
-
 let cell = document.createElement('div');
-cell.className='cell-item';
-cell.textContent='123';
-cell.style = 'display: flex; justify-content: center; align-items: center;';
-cell.style.backgroundColor='lightgrey';
-cell.style.border = 'solid 5px grey';
-cell.style.borderRadius = '5px';
+cell.classList.add('default-cell', 'cell-item');
 
+function refreshGrid(width,gridDiv){
+    // clears the grid of any previous child nodes
+    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    gridDiv.textContent = '';
+    // console.log('cells removed');
+    
+    // populates grid fully and keeps it a square
+    let nCells = width * width;
+    gridDiv.style='grid-template-columns: repeat('+width+',1fr);';
 
-
-// without cloneNode set to true to add multiple elements
-// https://javascript.tutorialink.com/how-to-appendchildelement-many-times-the-same-element/
-for (i=0; i<nCells; i++){
-    // number each cell
-    cell.textContent=''+(i+1)+''
-    container.appendChild(cell.cloneNode(true));
+    for (i=0; i<nCells; i++){
+        // without cloneNode set to true to add multiple elements
+        // https://javascript.tutorialink.com/how-to-appendchildelement-many-times-the-same-element/
+        gridDiv.appendChild(cell.cloneNode(true));
+        // console.log('cells added');
+    }
+    
+    // add eventListener to multiple items so each cell can change color
+    // https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
+    // Needs to be inside this function or it is not always called
+    const allCells = document.querySelectorAll('.cell-item');
+    allCells.forEach(item => { item.addEventListener('mouseover', randColor)})
 }
-
-// add eventListener to multiple items
-// https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
-const allCells = document.querySelectorAll('.cell-item');
-allCells.forEach(item => { item.addEventListener('mouseleave', randColor)})
 
 function randColor(e) {
     let colorRange = 256;
@@ -46,4 +40,20 @@ function randColor(e) {
     let b = Math.floor(Math.random()*colorRange);
     let c = Math.floor(Math.random()*colorRange);
     e.target.style.backgroundColor='rgb('+a+','+b+','+c+')';
+    console.log('rand color applied');
 }
+
+function buttonRefresh(){
+    let message = prompt('Enter a number from 1 to 100:');
+    
+    console.log('Inputted Value: '+message);
+    if (!isNaN(message) && message > 0 && message <= 100){
+        // console.log('is a number');
+        refreshGrid(message,container);        
+    } else {
+        // console.log('not a number');
+    }
+}
+
+// Default generation of grid
+refreshGrid(16,container);
